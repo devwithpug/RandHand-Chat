ff2cf47cd7ed0aca92a02db3abfa9b594379cf0b
DonghyunHan <mpolio9431@naver.com>
2021-06-30 16:17:38 +0900

    add generating coordinate code

--- mediaPipe/mediapipe_test.py
+++ mediaPipe/mediapipe_test.py
@@ -1,10 +1,14 @@
+import os
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # For static images:
-IMAGE_FILES = []
+path = 'C:/Users/dh/Desktop/Git/RandHand-Chat/mediaPipe/proto_image'
+os.chdir(path)
+IMAGE_FILES = os.listdir(path)
+
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=2,
@@ -22,46 +26,23 @@ with mp_hands.Hands(
       continue
     image_height, image_width, _ = image.shape
     annotated_image = image.copy()
+
+    f = open('../coordinate/c_'+str(idx)+'.txt','w')
     for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
+      f.write('\n x locate \n -------------------------\n')
+      for ids, landmrk in enumerate(hand_landmarks.landmark):
+        cx = str(landmrk.x)+'\n'
+        f.write(cx)
+      f.write('\n y locate \n -------------------------\n')
+      for ids, landmrk in enumerate(hand_landmarks.landmark):
+        cy = str(landmrk.y)+'\n'
+        f.write(cy)
+      f.write('\n z locate \n -------------------------\n')
+      for ids, landmrk in enumerate(hand_landmarks.landmark):
+        cz = str(landmrk.z)+'\n'
+        f.write(cz)
+            
       mp_drawing.draw_landmarks(
-          annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-
-# For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
-
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+        '../completion_image/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
