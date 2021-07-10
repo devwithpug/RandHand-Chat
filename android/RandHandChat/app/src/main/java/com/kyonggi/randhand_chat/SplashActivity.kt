b59593ccd84f04fae187613947d78b97e21f4985
joonhoo <mnc5@naver.com>
2021-07-10 11:59:32 +0900

    feature: 앱 실행시 Splash 화면 구현 [#5]

---
+++ android/RandHandChat/app/src/main/java/com/kyonggi/randhand_chat/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.kyonggi.randhand_chat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import androidx.constraintlayout.motion.widget.MotionLayout
+import java.util.logging.Handler
+
+class SplashActivity : AppCompatActivity() {
+//    private val SPLASH_VIEW_TIME: Long by lazy { 1500 }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val motionLayout: MotionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, SPLASH_VIEW_TIME)
+    }
+}
