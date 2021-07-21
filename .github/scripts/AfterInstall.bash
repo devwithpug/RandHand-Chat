b978b8651504d4c6708b595ee7bc033334fdc91d
devwithpug <zmfjscl789@gmail.com>
2021-07-21 23:09:56 +0900

    feature: Github Actions 워크플로우 추가 [#20]

--- .github/scripts/AfterInstall.bash
+++ .github/scripts/AfterInstall.bash
@@ -1,8 +1,8 @@
-docker pull devwithpug/eureka-server
-docker pull devwithpug/config-service
-docker pull devwithpug/gateway-service
-docker pull devwithpug/user-service
-docker pull devwithpug/chat-service
-docker pull devwithpug/gesture-service
+docker pull devwithpug/eureka-server:0.1
+docker pull devwithpug/config-service:0.1
+docker pull devwithpug/gateway-service:0.1
+docker pull devwithpug/user-service:0.1
+docker pull devwithpug/chat-service:0.1
+docker pull devwithpug/gesture-service:0.1
 
-docker-compose -f /home/ec2-user/docker-compose.yml up -d
+/usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d
