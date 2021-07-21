f6a8b2c1d161957839d0aefa4ed22f0fce269dee
devwithpug <zmfjscl789@gmail.com>
2021-07-22 03:13:55 +0900

    refactor: docker-compose startup 딜레이 추가 [#20]

--- .github/scripts/AfterInstall.bash
+++ .github/scripts/AfterInstall.bash
@@ -6,21 +6,21 @@ docker pull devwithpug/chat-service:0.1
 docker pull devwithpug/gesture-service:0.1
 
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d rabbitmq
-/usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml run --name wait-for-sec wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d config-service
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d eureka-server
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d gateway-service
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d zookeeper
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d kafka
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d user-service
-/usr/bin/docker start -a wait-for-sec
+/home/ec2-user/sleep.sh
 /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d chat-service
 # /usr/bin/docker start -a wait-for-sec
 # /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d gesture-service
-/usr/bin/docker stop wait-for-sec
-/usr/bin/docker rm wait-for-sec
+# /usr/bin/docker stop wait-for-sec
+# /usr/bin/docker rm wait-for-sec
