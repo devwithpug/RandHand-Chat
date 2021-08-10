e138e1eac69ccff922a3b6c8f712d9e336c786e1
devwithpug <zmfjscl789@gmail.com>
2021-08-10 19:42:27 +0900

    feature: kafka_consumer.py 제거 & gesture-service, mediapipe 연동 [#19]

---
+++ backend/gesture-service/app/gesture.py
@@ -0,0 +1,113 @@
+import numpy as np
+import base64
+import cv2
+import pandas as pd
+import mediapipe as mp
+
+class Gesture_Predict:
+
+    def __init__(self, limit=1):
+
+        # mp setting
+        mp_drawing = mp.solutions.drawing_utils
+        mp_hands = mp.solutions.hands
+        hands = mp_hands.Hands(
+            static_image_mode=True,
+            max_num_hands=2,
+            min_detection_confidence=0.5)
+
+        self.angle_data = pd.DataFrame(columns=['angle'+str(x) for x in range(0,18,1)])
+        self.limit = limit
+        self.hands = hands
+        self.predict = []
+
+
+    # =======================================================================================
+    # Compute Angles
+    # =======================================================================================
+    def compute_angles(self, img_list, user_ids):
+
+        for idx, file in enumerate(img_list):
+        
+            # img detection & preprocessing
+            img = cv2.flip(file, 1)
+            img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            result = self.hands.process(img2)
+
+            # result save
+            if result.multi_hand_landmarks is not None:
+                for res in result.multi_hand_landmarks:
+                    joint = np.zeros((21, 3))
+                    for j, lm in enumerate(res.landmark):
+                        joint[j] = [lm.x, lm.y, lm.z]
+
+                    # Compute angles between joints
+                    v1 = joint[[0,1,2,3,0,5,6,7,0, 9,10,11, 0,13,14,15, 0,17,18,19],:] # Parent joint
+                    v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+                    v = v2 - v1 # [20,3]
+
+                    # Normalize v
+                    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                    # Get angle using arcos of dot product
+                    angle = np.arccos(np.einsum('nt,nt->n',
+                        v[[0,5, 9,13],:], 
+                        v[[3,9,13,17],:]))
+                    angle2 = np.arccos(np.einsum('nt,nt->n',
+                        -v[[1,2,4,5,6,8, 9,10,12,13,14,16,17,18],:], 
+                        v[[2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
+
+                    angle = np.concatenate((angle, angle2),axis=0)
+
+                    # angle = np.degrees(angle) # Convert radian to degree
+                    self.angle_data.loc[str(user_ids[idx])] = angle
+
+
+    # =======================================================================================
+    # Custom Unsupervised Learning Algorithm
+    # =======================================================================================
+    def fit_predict(self, df):
+        distance = pd.DataFrame(columns=['x','y', 'value'])
+        count=1
+        for i in range(0,len(df.index),1):
+            for j in range(i+1,len(df.index),1):
+                row1=np.array(df.iloc[i,:])
+                row2=np.array(df.iloc[j,:])
+                distance=distance.append({'x': df.index[i], 'y': df.index[j],'value':((row1-row2)**2).sum()}, ignore_index=True)
+    
+        distance=distance[distance.value<self.limit] # 거리 제한
+
+        for k in distance.itertuples():
+            try:
+                x = distance.loc[distance.value.idxmin()]
+                self.predict.append([x.x, x.y])
+                count += 1
+                distance = distance[distance['x'] != x.x]
+                distance = distance[distance['x'] != x.y]
+                distance = distance[distance['y'] != x.x]
+                distance = distance[distance['y'] != x.y]
+            except:
+                break
+        return self.predict
+
+
+    def do_predict(self, gesture_dict):
+        img_list = []
+        # image decode (서버에서 base64 형식으로 packet이 오면, 형태를 이미지화 해줌)
+        for key in gesture_dict:
+            decoded = np.asarray(bytearray(base64.b64decode(gesture_dict[key])), dtype=np.uint8)
+            img = cv2.imdecode(decoded, cv2.IMREAD_COLOR)
+            img_list.append(img)
+
+        self.compute_angles(img_list, list(gesture_dict.keys()))
+
+        df = self.angle_data
+        thumb_false = df[df['angle0']*180/np.pi<90].iloc[:,1:]
+        thumb_true = df[df['angle0']*180/np.pi>=90].iloc[:,1:]
+        thumb_false.iloc[:,:3] *= 2
+        thumb_true.iloc[:,:3] *= 2
+
+        self.fit_predict(thumb_false)
+        self.fit_predict(thumb_true)
+
+        return self.predict
--- backend/gesture-service/app/gesture_service.py
+++ backend/gesture-service/app/gesture_service.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask_apscheduler import APScheduler, scheduler
 import py_eureka_client.eureka_client as eureka_client
-import socket, argparse, time
+import socket, argparse, time, json
 from contextlib import closing
-
+from kafka import KafkaProducer
+from gesture import Gesture_Predict
 
 def log(msg):
     print("[{}] {}".format(
@@ -10,12 +12,12 @@ def log(msg):
             msg
         ))
 
-
+gesture_dict = {}
 parser = argparse.ArgumentParser()
-parser.add_argument('eureka_server_ip', nargs='?', type=str, default='localhost:8761', help='ex) "localhost:8761"')
+parser.add_argument('host', nargs='?', type=str, default='localhost', help='ex) "localhost"')
 args = parser.parse_args()
 
-EUREKA_IP = args.eureka_server_ip
+EUREKA_IP = args.host
 
 rest_port = 5000
 
@@ -25,21 +27,78 @@ with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     rest_port = s.getsockname()[1]
 
-eureka_client.init(eureka_server=EUREKA_IP+"/eureka",
+rest_port = 5000
+
+eureka_client.init(eureka_server=EUREKA_IP+":8761/eureka",
                     app_name="gesture-service",
                     instance_port=rest_port)
 
 app = Flask(__name__)
+scheduler = APScheduler()
+scheduler.api_enabled = True
+scheduler.init_app(app)
+scheduler.start()
+
+producer = KafkaProducer(acks=0,
+                         compression_type='gzip', 
+                         bootstrap_servers=[EUREKA_IP+':9092'],
+                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+@scheduler.task('interval', id='predict_gesture', seconds=10)
+def do_predict():
+
+    predict = Gesture_Predict(gesture_dict, limit=1.5)
+    result = predict()
+
+    if len(result) > 0:
+
+        for ids in result:
+            gesture_dict.pop(ids[0])
+            gesture_dict.pop(ids[1])
+            log("New chat room created! : {}".format(ids))
+
+            value = {'userIds': ids}
+            producer.send('match-topic', value=value)
+
+        log("gesture_queue : {}".format(list(gesture_dict.keys())))
+
+# TODO - chat room 생성한뒤에 chat-service [match-topic] 전송까지 완료
+# TODO - chat-service 에서 [match-topic] consume하여 chatroom 싱글턴 리스트로 관리하고
+# TODO - [chat-topic] 으로 chatroom 전송 & 안드에서 컨슘하여 채팅방 접속
+# TODO - 안드에서 큐 삭제 요청이 오거나 채팅방 나가기 요청이 오는 경우 chatroom 리스트에서 sessionId삭제
 
 # create Queue
 @app.route("/queue", methods=['POST'])
 def create_queue():
 
-    # TODO - request data 검증 & 매칭 수행
+    gesture = request.json['gesture']
+    userId = request.headers['userId']
 
-    data = request.json
-    log(data)
-    return data
+    try:
+        assert isinstance(gesture, str)
+        assert isinstance(userId, str)
+    except AssertionError:
+        return Response(status=400)
+
+    gesture_dict[userId] = gesture
+
+    log("create queue : {}".format(userId))
+    log("gesture_queue : {}".format(list(gesture_dict.keys())))
+
+    return Response(status=200)
+
+@app.route("/queue/cancel", methods=['POST'])
+def cancel_queue():
+
+    userId = request.headers['userId']
+
+    try:
+        gesture_dict.pop(userId)
+    except KeyError:
+        return Response(status=404)
+
+    log("cancel queue : {}".format(userId))
+    return Response(status=200)
 
 # health_check
 @app.route("/health", methods= ['GET'])
