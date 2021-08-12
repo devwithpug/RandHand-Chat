6cc37b9765ce2d6320fb91e1b8254b2d6754a7a4
devwithpug <zmfjscl789@gmail.com>
2021-08-12 17:36:36 +0900

    refactor: RequestUser picture to Nullable [#30]

--- backend/chat-service/src/main/java/io/kgu/chatservice/domain/request/RequestUser.java
+++ backend/chat-service/src/main/java/io/kgu/chatservice/domain/request/RequestUser.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -26,14 +27,14 @@ public class RequestUser implements Serializable {
 
     @NotNull(message = "이메일이 존재하지 않습니다!")
     @Size(min = 2, message = "이메일은 최소 2자 이상이어야 합니다.")
-    @Email
+    @Email(message = "이메일 형식이 맞지 않습니다.")
     private String email;
 
     @NotNull(message = "이름이 존재하지 않습니다!")
     @NotBlank(message = "이름이 비어있습니다.")
     private String name;
 
-    @NotNull(message = "프로필 사진이 존재하지 않습니다!")
+    @URL(message = "프로필 이미지 형식이 맞지 않습니다.")
     private String picture;
 
     @Size(max = 50, message = "상태 메시지는 최대 50자 입니다.")
