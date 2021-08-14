17eca1ac4bba3d34162494876f481c6acebed73b
devwithpug <zmfjscl789@gmail.com>
2021-08-14 15:45:27 +0900

    chore: 테스트 프로젝트 RandHandServer 제거

--- backend/RandHandServer/src/main/java/io/kgu/randhandserver/repository/UserInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.kgu.randhandserver.repository;
-
-import io.kgu.randhandserver.domain.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-}
