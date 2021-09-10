f36311c12a769136e26e60efb2612bafe1a0eb89
devwithpug <zmfjscl789@gmail.com>
2021-09-11 00:17:10 +0900

    refactor: chat-service setup RabbitMQ & detach Kafka [#51]

---
+++ backend/chat-service/src/main/java/io/kgu/chatservice/messagequeue/RabbitMQConsumer.java
@@ -0,0 +1,34 @@
+package io.kgu.chatservice.messagequeue;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.kgu.chatservice.domain.dto.ChatDto;
+import io.kgu.chatservice.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RabbitMQConsumer {
+
+    private final ChatService chatService;
+    private final ObjectMapper objectMapper;
+
+    @RabbitListener(queues = {"match.queue"})
+    public void receiveMessage(final Message message) {
+        log.info(message.toString());
+        try {
+            ChatDto chatDto = objectMapper.readValue(message.getBody(), ChatDto.class);
+            chatService.createChatRoom(chatDto);
+            log.info(chatDto.getUserIds().toString());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+}
